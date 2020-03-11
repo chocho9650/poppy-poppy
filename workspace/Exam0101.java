1b1996d7465d0d35632e22bd81b3fb07dbc45904
kim <kikfudxo3345@naver.com>
2020-03-11 17:42:54 +0900

    Add workspace

---
+++ workspace/Exam0101.java
@@ -0,0 +1,13 @@
+/**
+ * @Filename    : Exam0101.java
+ * @Description : 자신의 연도에 따른 나이를 출력한다.
+ * @Author      : 김령태 (fudxo3345@gmail.com)
+ */
+public class Exam0101 {
+	public static void main(String[] args) {
+		int year = 1995;
+		int age = 2020 - year + 1;
+
+		System.out.println(age + "세 입니다.");
+	}
+}
